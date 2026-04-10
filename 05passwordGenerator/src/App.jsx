d93f5528f4041a6d52cf264636a0630b989c672f
ShagunMishra640 <shagunmishra640@gmail.com>
2026-04-10 13:57:08 +0530

    password generator

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,94 @@
+import { useState, useCallback, useEffect, useRef} from 'react'
+
+
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  //useRef hook
+  const passwordRef = useRef(null)
+
+  {/*useCallback react ka hook hai jo function ko memoize karta hai ki function tab tak re-create na ho jab tak uski dependency change  na ho.iska main use performance optimization ke liye hota hai, especially jab hum function ko child components me  pass karte hai */}
+  const PasswordGenerator = useCallback(()=>{
+    let pass =""
+    let str ="ABCDEFGHIJKLMNOPQRSTUVWXYXabcdefghijklmnopqrstuvwxyz"
+
+    if(numberAllowed){str+= "0123456789" }
+    if(charAllowed){ str+="!@$%^&*_-+=~`" }
+
+    for (let i = 1; i<= length ;i++) {
+      let char = Math.floor(Math.random() * str.length +1)
+      pass+= str.charAt(char)
+    }
+     
+    setPassword(pass)
+
+  }, [length,numberAllowed,charAllowed])
+
+
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select(); // it is to give that howering effect on selected text
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+    {/*ye ham function ko runkarne ke use kar rahe  hai direct run karne pe error ayega kyuki react me continuous re rendering hoti rahengi isiliye ham iske use karenge.ye function ko tabhi run karega jab dependency me kuch change hoga */}
+   useEffect(()=>{
+    PasswordGenerator()
+   },[length, numberAllowed, charAllowed, PasswordGenerator])
+  return (
+    <>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+      <h1 className='text-white text-center my-3'>password generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input type="text"
+        value = {password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='password' 
+        readOnly
+        ref={passwordRef}/>
+        <button 
+        onClick={copyPasswordToClipboard}
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+
+
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input type="range"
+          min ={6}
+          max={100}
+         value={length}
+          className='cursor-pointer'
+          onChange={(e)=>{setLength(e.target.value)}}
+          />
+          <label >Length: {length}</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox"
+          defaultChecked={numberAllowed}
+          id='numberInput'
+          onChange={()=>{
+            setNumberAllowed((prev)=>!prev);
+          }} />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox"
+          defaultChecked={charAllowed}
+          id='characterInput'
+          onChange={()=>{
+            setCharAllowed((prev)=>!prev);
+          }} />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+       
+      </div>
+    </div>
+    </>
+  ) 
+}
+
+export default App
